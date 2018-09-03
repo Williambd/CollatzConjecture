da9460c452e0e0e58708d089a1a43a0f66fc8923
Liam <williambradydeaton@gmail.com>
2018-09-03 11:13:00 -0400

    Added file reading for saving and continuing over time

--- CollatzConjecture.cpp
+++ CollatzConjecture.cpp
@@ -4,20 +4,36 @@ to attempt to disprove the collatz conjecture.
 https://en.wikipedia.org/wiki/Collatz_conjecture*/
 #include <stdio.h>
 #include <time.h>
+#include <fstream>
 
+using namespace std;
 
 int main()
 {
+	//gets the last calculation made by program
+	ifstream myfile;
+	ofstream ofs;
+	int n;
+	myfile.open("lastcalc.txt");
+	int firstCalc;
+	string firstCalcStr;
+	//assigns line to string then transfers to int
+	getline(myfile,firstCalcStr);
+	firstCalc = stoi(firstCalcStr);
+	myfile.close();
 
-	int testN = 2;
-	int n = testN;
+	int testN = firstCalc;
 	time_t start;
-	printf("Minutes until Collatz process should be terminated:");
+
+	//gets the minutes until collatz process should be terminated in minutes.
+	printf("Minutes of runtime until Collatz process should be terminated and file printed:");
 	int killT;
 	scanf("%d", &killT);
 	killT = killT*60;
+	
 	while(true)
 	{
+
 		start = time(NULL);
 		n = testN;
 		while (n != 1){
@@ -32,7 +48,11 @@ int main()
 				break;
 			}
 		}
+		ofs.open("lastcalc.txt", ofstream::out | ofstream::trunc);
+		ofs << testN;
+		ofs.close();
 		testN++;
 	}	
+	return 0;
 
 }
